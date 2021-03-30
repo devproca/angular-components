bc6836f1377e880888178731c3ca288d3661709b
Colin <colin@dev-pro.ca>
2021-03-30 11:57:15 -0600

    Added info, warning, and danger alerts + styling.

--- src/app/alert/alert.service.ts
+++ src/app/alert/alert.service.ts
@@ -8,12 +8,41 @@ export class AlertService {
 
   constructor(private factoryResolver: ComponentFactoryResolver) { }
 
+  // TODO: MOVE REPETITIVE CODE TO ANOTHER FUNCTION!!!!!!
+
   success(containerRef: ViewContainerRef, inputMessage: string): void {
     containerRef.clear();
     const factory = this.factoryResolver.resolveComponentFactory(AlertComponent);
     const componentRef = containerRef.createComponent(factory);
 
     componentRef.instance.type = 'success';
+    componentRef.instance.message = inputMessage;
+  }
+
+  info(containerRef: ViewContainerRef, inputMessage: string): void {
+    containerRef.clear();
+    const factory = this.factoryResolver.resolveComponentFactory(AlertComponent);
+    const componentRef = containerRef.createComponent(factory);
+
+    componentRef.instance.type = 'info';
+    componentRef.instance.message = inputMessage;
+  }
+
+  warning(containerRef: ViewContainerRef, inputMessage: string): void {
+    containerRef.clear();
+    const factory = this.factoryResolver.resolveComponentFactory(AlertComponent);
+    const componentRef = containerRef.createComponent(factory);
+
+    componentRef.instance.type = 'warning';
+    componentRef.instance.message = inputMessage;
+  }
+
+  danger(containerRef: ViewContainerRef, inputMessage: string): void {
+    containerRef.clear();
+    const factory = this.factoryResolver.resolveComponentFactory(AlertComponent);
+    const componentRef = containerRef.createComponent(factory);
+
+    componentRef.instance.type = 'danger';
     componentRef.instance.message = inputMessage;
   }
 }
--- src/app/demo-alert/demo-alert.component.ts
+++ src/app/demo-alert/demo-alert.component.ts
@@ -13,11 +13,17 @@ import { AlertService } from '../alert/alert.service';
 })
 export class DemoAlertComponent implements AfterViewInit{
 
-  @ViewChild('alert', { read: ViewContainerRef }) alert: ViewContainerRef;
+  @ViewChild('alertSuccess', { read: ViewContainerRef }) alertSuccess: ViewContainerRef;
+  @ViewChild('alertInfo', { read: ViewContainerRef }) alertInfo: ViewContainerRef;
+  @ViewChild('alertWarning', { read: ViewContainerRef }) alertWarning: ViewContainerRef;
+  @ViewChild('alertDanger', { read: ViewContainerRef }) alertDanger: ViewContainerRef;
 
   constructor(private alertService: AlertService) { }
 
   ngAfterViewInit(): void {
-    this.alertService.success(this.alert, 'You have succeeded at your task. This is a success message that can be longer!');
+    this.alertService.success(this.alertSuccess, 'You have succeeded at your task. This is a success message that can be longer!');
+    this.alertService.info(this.alertInfo, 'Pay attention to this info before proceeding!');
+    this.alertService.warning(this.alertWarning, 'Something you have done is causing a warning!');
+    this.alertService.danger(this.alertDanger, 'Make changes before you submit again, something went wrong!');
   }
 }
